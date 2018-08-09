e0dcccfbe8ae61d91e91c1e440f6690a55f3b312
Ashwin <ashwin.dinesh01@gmail.com>
2018-08-09 21:18:01 +0530

    initial commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-webengage.WebEngagePlugin",
+    "file": "plugins/cordova-plugin-webengage/www/WebEngagePlugin.js",
+    "pluginId": "cordova-plugin-webengage",
+    "clobbers": [
+      "webengage"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-webengage": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
